8d56dafce23ef76f3ea7bb39fa193279ba8d92b7
Jacek Pasierb <jack_byk@o2.pl>
2026-02-19 15:12:11 +0100

    first commit

--- app/admin/login/loginForm.tsx
+++ app/admin/login/loginForm.tsx
@@ -4,12 +4,16 @@ import {useState} from "react";
 import {signIn} from "next-auth/react";
 import {useSearchParams} from "next/navigation";
 
-
-
-import { Button } from "../../../components/ui/button";
-import { Input } from "../../../components/ui/input";
-import { Label } from "../../../components/ui/label";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../../../components/ui/card";
+import {Input} from "../../../components/ui/input";
+import {Label} from "../../../components/ui/label";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "../../../components/ui/card";
+import {Button} from "../../../components/ui/button";
 
 export default function LoginForm() {
   const sp = useSearchParams();
