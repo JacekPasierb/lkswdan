62520b6a5bb332becf912e881d1f30651fb11beb
Jacek Pasierb <jack_byk@o2.pl>
2026-02-20 14:30:00 +0100

    first commit

--- app/admin/login/loginForm.tsx
+++ app/admin/login/loginForm.tsx
@@ -11,9 +11,9 @@ import {
   CardHeader,
   CardTitle,
 } from "../../../components/ui/card";
-import { Label } from "../../../components/ui/label";
-import { Input } from "../../../components/ui/input";
-import { Button } from "../../../components/ui/button";
+import {Button} from "@/components/ui/button";
+import {Input} from "@/components/ui/input";
+import {Label} from "@/components/ui/label";
 
 export default function LoginForm() {
   const sp = useSearchParams();
