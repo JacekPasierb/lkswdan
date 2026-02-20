68fd42fd79d0fc9ef8a5760e8ef45e1d638cff1f
Jacek Pasierb <jack_byk@o2.pl>
2026-02-20 14:56:53 +0100

    Fix - add load for i18n.ts

--- components/admin/Sidebar.tsx
+++ components/admin/Sidebar.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import {usePathname} from "next/navigation";
 import {cn} from "@/lib/utils";
-import { Separator } from "@/components/ui/seperator";
+import { Separator } from "@/components/ui/separator";
 
 
 const items = [
