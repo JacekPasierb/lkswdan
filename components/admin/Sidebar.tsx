5955414acf8ee65ea5157432d074684495bc47e3
Jacek Pasierb <jack_byk@o2.pl>
2026-02-20 14:35:54 +0100

    Fix ui component filenames casing

--- components/admin/Sidebar.tsx
+++ components/admin/Sidebar.tsx
@@ -3,7 +3,8 @@
 import Link from "next/link";
 import {usePathname} from "next/navigation";
 import {cn} from "@/lib/utils";
-import {Separator} from "@/components/ui/separator";
+import { Separator } from "@/components/ui/seperator";
+
 
 const items = [
   {href: "/admin", label: "Dashboard"},
